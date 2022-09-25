c722d272470bd570fbf9b176d343aa8102addb68
romankravchuk <kravchuk.roman02@gmail.com>
2022-09-25 15:37:30 +0800

    Replace UserSerializer to serializers.py

--- src/toronto/models.py
+++ src/toronto/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "is_staff"]
--- src/toronto/views.py
+++ src/toronto/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from . import models
+from . import serializers
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = models.UserSerializer
+    serializer_class = serializers.UserSerializer
