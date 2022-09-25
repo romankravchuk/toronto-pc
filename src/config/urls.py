22f5882ca167dc3a06617ac115db9a04b22b21f9
romankravchuk <kravchuk.roman02@gmail.com>
2022-09-25 15:55:44 +0800

    Add UserProfile

--- src/config/urls.py
+++ src/config/urls.py
@@ -8,6 +8,6 @@ router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("api", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
