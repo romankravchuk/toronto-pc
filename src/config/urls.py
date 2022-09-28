17746ee19f0312f40f0b70dbb083a2bc2a8a8d23
romankravchuk <kravchuk.roman02@gmail.com>
2022-09-28 18:24:39 +0800

    Replace toronto urls to urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,17 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from rest_framework import routers
-
-from toronto import views
-
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
+    path("api/", include("toronto.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
 
