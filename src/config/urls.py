5566a980b8890f2965d58910aa74cd050f8e87a6
romankravchuk <kravchuk.roman02@gmail.com>
2022-10-04 18:18:16 +0800

    Init api

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,13 +1,5 @@
 from django.urls import path, include
-from rest_framework import routers
-
-from toronto import views
-
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
-    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- src/toronto/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-
-class UserProfileTest(TestCase):
-    pass
